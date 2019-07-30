e5c1ae56659b0154e61ce546395828e8cb615c41
Kyle Baker <tokylebaker@gmail.com>
2019-07-29 19:02:05 -0700

    insert

--- resizing_hashtable/r_hashtables.py
+++ resizing_hashtable/r_hashtables.py
@@ -17,48 +17,59 @@ class LinkedPair:
 # '''
 class HashTable:
     def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = [None]*capacity
+
+    # '''
+    # Research and implement the djb2 hash function
+    # '''
+    def hash(string, max):
+        hash = 5381
+
+        for character in string:
+            hash = ((hash << 5) + hash) + ord(character)
+
+        return hash % max
+
+    # '''
+    # Fill this in.
+
+    # Hint: Used the LL to handle collisions
+    # '''
+    def hash_table_insert(self, hash_table, key, value):
+        key_hash = hash(key, self.capacity)
+
+        if self.storage[key_hash] is None:
+            self.storage[key_hash] = LinkedPair(key, value)
+        else:
+            pair = self.storage[key_hash]
+            last_pair = self.storage[key_hash]
+
+            while pair is not None and pair.key != key:
+                if pair.next is not None:
+                    last_pair = pair.next
+                pair = pair.next
+
+            if pair is None:
+                last_pair.next = LinkedPair(key, value)
+            else:
+                pair.value = value
+
+    # If you try to remove a value that isn't there, print a warning.
+    # '''
+    def hash_table_remove(hash_table, key):
         pass
 
+    # Should return None if the key is not found.
+    # '''
+    def hash_table_retrieve(hash_table, key):
+        pass
 
-# '''
-# Research and implement the djb2 hash function
-# '''
-def hash(string, max):
-    pass
-
-
-# '''
-# Fill this in.
-
-# Hint: Used the LL to handle collisions
-# '''
-def hash_table_insert(hash_table, key, value):
-    pass
-
-
-# '''
-# Fill this in.
-
-# If you try to remove a value that isn't there, print a warning.
-# '''
-def hash_table_remove(hash_table, key):
-    pass
-
-
-# '''
-# Fill this in.
-
-# Should return None if the key is not found.
-# '''
-def hash_table_retrieve(hash_table, key):
-    pass
-
-
-# '''
-# Fill this in
-# '''
-def hash_table_resize(hash_table):
-    pass
+    # '''
+    # Fill this in
+    # '''
+    def hash_table_resize(hash_table):
+        pass
 
 
 def Testing():
